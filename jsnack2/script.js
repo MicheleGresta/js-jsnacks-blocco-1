5875e609319b3823f78c992d972f9acf628a7f90
MicheleGresta <malachia_1997@hotmail.it>
2023-07-06 15:36:10 +0200

    esercizio 2 un po macchinoso

---
+++ jsnack2/script.js
@@ -0,0 +1,8 @@
+
+const listaNumeri = [3,5,3,5,3,5,3,5,3,5,3];
+
+for (let i = 1; i < 2; i += 2) {
+    sommaNumeri = listaNumeri[i] + (listaNumeri[i+2]) + (listaNumeri[i+2]) + (listaNumeri[i+2]) + (listaNumeri[i+2]);
+}
+
+console.log(sommaNumeri);
