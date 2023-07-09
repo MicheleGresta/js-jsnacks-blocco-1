72be2c3b3a72859eaa702f26db0246ed40747365
MicheleGresta <malachia_1997@hotmail.it>
2023-07-09 07:57:52 +0200

    correzione es

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,10 +1,12 @@
 const listaNomi = ["michele","matteo","mattia","francesco","marco"];
 const listaCognomi = ["bianchi", "verdi", "neri" , "rossi", "marrone"];
+const listaPersone = [];
 
 for (i = 0; i < listaNomi.length; i++) {
-    console.log(listaNomi[Math.round(Math.random()*[i])] + " " +listaCognomi[Math.round(Math.random()*[i])])
+    listaPersone.push(listaNomi[Math.round(Math.random()*[i])] + " " +listaCognomi[Math.round(Math.random()*[i])]);
 }
 
 
 
 
+console.log(listaPersone);
